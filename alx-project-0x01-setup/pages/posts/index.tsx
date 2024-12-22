f064defe653aca8a144c14a4894f1e4e1cc60c5c
Godfred <essiengodfred88@gmail.com>
2024-12-22 18:19:07 +0000

    Set up navigation between pages with header and links

--- alx-project-0x01-setup/pages/posts/index.tsx
+++ alx-project-0x01-setup/pages/posts/index.tsx
@@ -1,11 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
-    return (
-      <div>
-        <h2 className="text-4xl font-semibold">Posts</h2>
-        {/* Render a list of PostCard components here */}
-      </div>
-    );
-  };
-  
-  export default PostsPage;
-  
+  return (
+    <div>
+      <Header />
+      <h2 className="text-4xl font-semibold text-center mt-8">Posts</h2>
+      {/* Render list of posts here */}
+    </div>
+  );
+}
+
+export default PostsPage;
--- alx-project-0x01-setup/pages/users/index.tsx
+++ alx-project-0x01-setup/pages/users/index.tsx
@@ -1,11 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
-    return (
-      <div>
-        <h2 className="text-4xl font-semibold">Users</h2>
-        {/* Render user profiles here */}
-      </div>
-    );
-  };
-  
-  export default UsersPage;
-  
+  return (
+    <div>
+      <Header />
+      <h2 className="text-4xl font-semibold text-center mt-8">Users</h2>
+      {/* Render list of users here */}
+    </div>
+  );
+}
+
+export default UsersPage;
