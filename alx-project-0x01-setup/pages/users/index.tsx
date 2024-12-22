2da2c5780fc2e8e27febe6520a79c5e1116d7d57
Godfred <essiengodfred88@gmail.com>
2024-12-22 18:41:00 +0000

    Added User Modal component with dynamic functionality for adding new users.

--- alx-project-0x01-setup/pages/users/index.tsx
+++ alx-project-0x01-setup/pages/users/index.tsx
@@ -1,45 +1,49 @@
 import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
 import Header from "@/components/layout/Header";
-import { UserProps } from "@/interfaces";
+import { UserData, UserProps } from "@/interfaces";
+import { useState } from "react";
 
-const Users: React.FC<UserProps[]> = ({ posts }) => {
-  console.log(posts)
+const Users: React.FC<UserProps[]> = ({ users }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser, id: users.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-semibold">User Information</h1>
+          <h1 className=" text-2xl font-semibold">User Management</h1>
+          <button onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-3 gap-2">
           {
-            posts?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
-              <UserCard 
-                id={id} 
-                name={name} 
-                username={username} 
-                email={email} 
-                address={address} 
-                phone={phone} 
-                website={website} 
-                company={company} 
-                key={key} 
-              />
+            users?.map(({ name, username, email, id }: UserProps, key: number) => (
+              <UserCard name={name} username={username} email={email} id={id} key={key} />
             ))
           }
         </div>
       </main>
+
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
     </div>
   )
 }
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const users = await response.json()
 
   return {
     props: {
-      posts
+      users
     }
   }
 }
