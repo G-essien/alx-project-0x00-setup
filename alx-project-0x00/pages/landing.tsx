5ddf194e6c20080044dcf268259bbaa2a0759104
Godfred <essiengodfred88@gmail.com>
2024-12-22 17:27:29 +0000

    Added Card component with dynamic Pill components and integrated into the Landing page.

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,14 @@
+
+import React from "react";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
     return (
       <div>
-        <h1 className="text-xl font-extralight">Landing Page</h1>
+      <h1 className="text-xl font-extralight">Welcome to Our Landing Page</h1>
+        <Card title="Beautiful House 1" />
+        <Card title="Stunning Villa 2" />
+        <Card title="Modern Apartment 3" />
       </div>
     );
   };
